43896c5e3f6753993e6621d7ab2e5b67c43bd986
ejiro-edwin <ejiroedwin@gmail.com>
2021-01-19 21:37:31 +0100

    sessions and sendmail

--- src/startServer.ts
+++ src/startServer.ts
@@ -1,20 +1,85 @@
-import { importSchema } from "graphql-import";
+import "reflect-metadata";
+import "dotenv/config";
 import { GraphQLServer } from "graphql-yoga";
-import * as path from "path";
+import * as session from "express-session";
+import * as connectRedis from "connect-redis";
+import * as RateLimit from "express-rate-limit";
+import * as RateLimitRedisStore from "rate-limit-redis";
 
-import { resolvers } from "./resolvers";
-import { createTypeOrmConnection } from "./utils/createTypeOrmConnection";
-// ... or using `require()`
-// const { GraphQLServer } = require('graphql-yoga')
+import { redis } from "./redis";
+import { createTypeormConn } from "./utils/createTypeormConn";
+import { confirmEmail } from "./routes/confirmEmail";
+import { genSchema } from "./utils/genSchema";
+import { redisSessionPrefix } from "./constants";
+import { createTestConn } from "./testUtils/createTestConn";
 
-// or .gql or glob pattern like **/*.graphql
+const SESSION_SECRET = "ajslkjalksjdfkl";
+const RedisStore = connectRedis(session as any);
 
 export const startServer = async () => {
-  const typeDefs = importSchema(path.join(__dirname, "./schema.graphql"));
-  const server = new GraphQLServer({ typeDefs, resolvers });
-   await createTypeOrmConnection()
-   const app = await server.start({port:process.env.NODE_ENV ==="test" ? 0 : 4000});
-   console.log("Server is running on localhost:4000")
+  if (process.env.NODE_ENV === "test") {
+    await redis.flushall();
+  }
 
-   return app;
+  const server = new GraphQLServer({
+    schema: genSchema() as any,
+    context: ({ request }) => ({
+      redis,
+      url: request.protocol + "://" + request.get("host"),
+      session: request.session,
+      req: request
+    })
+  });
+
+  server.express.use(
+    new RateLimit({
+      store: new RateLimitRedisStore({
+        client: redis
+      }),
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+      delayMs: 0 // disable delaying - full speed until the max limit is reached
+    })
+  );
+
+  server.express.use(
+    session({
+      store: new RedisStore({
+        client: redis as any,
+        prefix: redisSessionPrefix
+      }),
+      name: "qid",
+      secret: SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
+      }
+    } as any)
+  );
+
+  const cors = {
+    credentials: true,
+    origin:
+      process.env.NODE_ENV === "test"
+        ? "*"
+        : (process.env.FRONTEND_HOST as string)
+  };
+
+  server.express.get("/confirm/:id", confirmEmail);
+
+  if (process.env.NODE_ENV === "test") {
+    await createTestConn(true);
+  } else {
+    await createTypeormConn();
+  }
+  const app = await server.start({
+    cors,
+    port: process.env.NODE_ENV === "test" ? 0 : 4000
+  });
+  console.log("Server is running on localhost:4000");
+
+  return app;
 };
